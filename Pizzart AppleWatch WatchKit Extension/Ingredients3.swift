ef9486455f52db7cdbd1b73077546e00d638edf3
Mateo Villagomez <Mateo@Mateos-MacBook-Pro.local>
2016-01-18 13:20:25 -0500

    final

--- Pizzart AppleWatch WatchKit Extension/Ingredients3.swift
+++ Pizzart AppleWatch WatchKit Extension/Ingredients3.swift
@@ -11,29 +11,42 @@ import Foundation
 
 class Ingredients3ViewController: WKInterfaceController {
     
+    var itemArray = []
+    
     @IBOutlet var picker: WKInterfacePicker!
+   
+    @IBOutlet var Order: WKInterfaceButton!
     
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
         let item1 = WKPickerItem()
-        item1.title = "Ham"
+        item1.title = ""
         
         let item2 = WKPickerItem()
-        item2.title = "Pepper"
+        item2.title = "Ham"
         
         let item3 = WKPickerItem()
-        item3.title = "Tomatoes"
+        item3.title = "Pepper"
         
         let item4 = WKPickerItem()
-        item4.title = "Pepperoni"
+        item4.title = "Tomatoes"
         
-        let itemArrays = [item1, item2, item3, item4]
+        let item5 = WKPickerItem()
+        item5.title = "Pepperoni"
         
-        picker.setItems(itemArrays)
+        self.itemArray = [item1, item2, item3, item4, item5]
         
+        picker.setItems(itemArray as? [WKPickerItem])
     }
-    override func willActivate() {
+
+    
+    @IBAction func Next() {
+    
+        pushControllerWithName("Enjoy It", context: Ingredients3ViewController.self)
+    
+    }
+      override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
     }
